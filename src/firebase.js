6868dd9edaf59d84fe118c6a418959c670c0d7d0
Minjun <ilovegod0510@gmail.com>
2022-01-31 03:56:34 +0900

    2022/01/31

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCN9_67MnTHYsXAmhCI4GDV2d_iFezMnhU",
+    authDomain: "netflix-clone-13a38.firebaseapp.com",
+    projectId: "netflix-clone-13a38",
+    storageBucket: "netflix-clone-13a38.appspot.com",
+    messagingSenderId: "943734395329",
+    appId: "1:943734395329:web:e5c964a8dc62936ad21f40"
+  };
